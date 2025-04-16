cdc5f366f8a4b41aee194eb43f6ae1b4e668ccd7
wizzamabob <wizzamabob@gmail.com>
2025-04-16 03:32:16 +0000

    Insertion Fix

--- switching-program/src/components/ProgramTable.js
+++ switching-program/src/components/ProgramTable.js
@@ -9,7 +9,7 @@ import redX from './red_x.png'; // Ensure the path is correct
 
 const ItemType = 'ROW';
 
-const DraggableRow = React.memo(({ row, index, moveRow, handleInputChange, deleteRow, itemNumber, isReverseSection, columnWidths, onRowClick, isScrolling, isCurrentlyDragging }) => {
+const DraggableRow = React.memo(({ row, index, moveRow, handleInputChange, deleteRow, itemNumber, isReverseSection, columnWidths, onRowClick, isScrolling, isDragging }) => {
   const ref = useRef(null);
   const [{ isDragging }, drag] = useDrag({
     type: ItemType,
@@ -499,7 +499,7 @@ const ProgramTable = ({ tableData, setTableData, formData }) => {
                     columnWidths={columnWidths}
                     onRowClick={handleRowClick} // Pass the click handler
                     isScrolling={isScrolling}
-                    isCurrentlyDragging={isDragging} // Changed isDragging to isCurrentlyDragging
+                    isDragging={isDragging} // Changed isDragging to isCurrentlyDragging
                   />
                 );
               })}
