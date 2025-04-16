4c006cc18a21d2a60f82a12ab34f18284c012881
wizzamabob <wizzamabob@gmail.com>
2025-04-16 05:10:51 +0000

    Logo Fix

--- switching-program/src/components/Header.js
+++ switching-program/src/components/Header.js
@@ -4,7 +4,7 @@ import '../styles.css';  // Ensure styles are imported to apply the custom style
 const Header = () => (
   <header className="header">
     <div className="header-content">
-      <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt="Logo" className="logo" style={{ width: '300px', height: '150px' }} /> {/* Adjust the width and height values as needed */}
+      <img src={`${process.env.PUBLIC_URL}/logo.jpg`} alt="Logo" className="logo" style={{ width: '150px', height: '150px' }} /> {/* Adjust the width and height values as needed */}
       <h1 className="title">High Voltage Switching Program Creator</h1>
     </div>
   </header>
